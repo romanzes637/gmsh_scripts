714cad07a4d6680a6a7aa909ba73668bcd3f3850
romanzes637 <romanbutov637@gmail.com>
2021-05-10 21:29:50 +0300

    optimize update

--- optimize.py
+++ optimize.py
@@ -1,10 +1,16 @@
 import gmsh
+import sys
 
 
 if __name__ == '__main__':
+    print(f"script: {sys.argv[0]}")
+    print(f"input: {sys.argv[1]}")
+    print(f"output: {sys.argv[2]}")
+    n_i = int(sys.argv[3]) if len(sys.argv) == 4 else 1
+    print(f'iterations: {n_i}')
     gmsh.initialize()
-    fn = 'test_cylinder_config.msh2'
-    gmsh.open(fn)
+    gmsh.option.setNumber("General.Terminal", 1)
+    gmsh.open(sys.argv[1])
     # "" for default tetrahedral mesh optimizer
     # "Netgen" for Netgen optimizer,
     # "HighOrder" for direct high-order mesh optimizer,
@@ -12,6 +18,6 @@ if __name__ == '__main__':
     # "HighOrderFastCurving" for fast curving algorithm,
     # "Laplace2D" for Laplace smoothing,
     # "Relocate2D" and "Relocate3D" for node relocation
-    gmsh.model.mesh.optimize("", force=True, niter=1)
-    gmsh.write('test_cylinder_config_opt.msh2')
+    gmsh.model.mesh.optimize("", force=True, niter=n_i)
+    gmsh.write(sys.argv[2])
     gmsh.finalize()
