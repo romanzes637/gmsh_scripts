8ba2de15d1f9a87fc8ede6afd8522496f55c866f
romanzes637 <romanbutov637@gmail.com>
2022-02-01 23:31:57 +0300

    coaction routine

--- src/ml/action/coaction.py
+++ src/ml/action/coaction.py
@@ -114,17 +114,17 @@ class Coaction(Action):
                         n += len(self.sub_actions) * self.sub_jobs - u
 
     def __call__(self, stack_trace=None, *args, **kwargs):
-        st = [self] if stack_trace is None else stack_trace + [self]
-        print(f'{os.getpid()} {" <- ".join([x.uid if x.tag is None else x.tag for x in st[::-1]])}, '
-              f'{self.executor if self.executor is not None else "SequenceExecutor"}/'
-              f'{self.sub_executor if self.sub_executor is not None else "SequenceExecutor"}, '
-              f'{self.executor_kwargs.get("max_workers", "max")}/'
-              f'{self.sub_executor_kwargs.get("max_workers", "max")} worker(s), '
-              f'{self.jobs if self.jobs is not None else "inf"}/'
-              f'{self.sub_jobs if self.jobs is not None else "inf"} job(s), '
-              f'{self.timeout if self.timeout is not None else "inf"}/'
-              f'{self.sub_timeout if self.sub_timeout is not None else "inf"} timeout, '
-              f'{type(self)}')
+        # st = [self] if stack_trace is None else stack_trace + [self]
+        # print(f'{os.getpid()} {" <- ".join([x.uid if x.tag is None else x.tag for x in st[::-1]])}, '
+        #       f'{self.executor if self.executor is not None else "SequenceExecutor"}/'
+        #       f'{self.sub_executor if self.sub_executor is not None else "SequenceExecutor"}, '
+        #       f'{self.executor_kwargs.get("max_workers", "max")}/'
+        #       f'{self.sub_executor_kwargs.get("max_workers", "max")} worker(s), '
+        #       f'{self.jobs if self.jobs is not None else "inf"}/'
+        #       f'{self.sub_jobs if self.jobs is not None else "inf"} job(s), '
+        #       f'{self.timeout if self.timeout is not None else "inf"}/'
+        #       f'{self.sub_timeout if self.sub_timeout is not None else "inf"} timeout, '
+        #       f'{type(self)}')
         if self.executor is None:
             super().__call__(stack_trace=stack_trace, *args, **kwargs)
         else:
